d1354628ed6de97c8f368de7eb9c85a9967bb88e
Reza A'masyi <mnurrreza@gmail.com>
2023-01-29 16:12:43 +0700

    add(.config): `chemacs2` submodule and profiles.el config

---
+++ .config/chemacs/profiles.el
@@ -0,0 +1,9 @@
+(("capture" . ((user-emacs-directory . "~/.config/chemacs/dotemacsen/capture")
+	       (server-name . "capture")
+	       ))
+
+;; emacs distribution: DOOM-emacs
+("doom" . ((user-emacs-directory . "~/.config/chemacs/dotemacsen/doom")
+	   (server-name . "server")
+	   (env . (("DOOMDIR" . "~/.config/doom")))
+	     ))
